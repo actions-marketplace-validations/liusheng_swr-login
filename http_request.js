83e6e7f09cba0e97c1799f0f52df103a28be7153
liusheng <liusheng2048@gmail.com>
2021-03-25 11:52:40 +0800

    Debug swr-login action

--- http_request.js
+++ http_request.js
@@ -3,11 +3,6 @@ const qs = require('querystring')
 const signer = require('./signer')
 const sig = new signer.Signer()
 
-module.exports = {
-  init_sign,
-  http_request
-}
-
 /**
  *  Set the AK/SK to sign and authenticate the request.
  * @param {*} config { AccessKey, SecretKey }
@@ -18,48 +13,28 @@ function init_sign(config) {
 }
 
 async function http_request(method, url, query, body) {
-  if (process.env.HW_HTTP_DEBUG === 'true') {
-    console.debug(`===http_request begin===`)
-    console.debug(`${method} - ${url}`)
-    if (query || body) {
-      console.debug(`${JSON.stringify({ query, body })}`)
-    }
-    console.debug(`======================`)
-  }
   try {
     let resp = await __http_request(method, url, query, body)
-
-    if (process.env.HW_HTTP_DEBUG === 'true') {
-      console.debug(`===http_request end===`)
-      console.debug(`${method} - ${url}`)
-      if (resp) {
-        console.debug(`${JSON.stringify(resp)}`)
-      }
-      console.debug(`====================`)
-    }
-
     return resp
   } catch (err) {
-    if (process.env.HW_HTTP_DEBUG === 'true') {
-      console.error(`===http_request error===`)
-      console.error(`${method} - ${url}`)
-      if (err.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.error(err.response.data)
-        console.error(err.response.status)
-        console.error(err.response.headers)
-      } else if (err.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.error(err.request)
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.error(err.message)
-      }
-      console.error(`======================`)
+    console.error(`===http_request error===`)
+    console.error(`${method} - ${url}`)
+    if (err.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error(err.response.data)
+      console.error(err.response.status)
+      console.error(err.response.headers)
+    } else if (err.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.error(err.request)
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error(err.message)
     }
+    console.error(`======================`)
     throw err
   }
 }
@@ -79,8 +54,6 @@ async function __http_request(method, url, query, body) {
     url,
     headers: r.headers
   }
-  console.debug(`liusheng-----------------------debug----`)
-  console.debug(`${JSON.stringify(r.headers)} - ${JSON.stringify(reqOpts)}`)
   if (query) {
     reqOpts.params = query
     reqOpts.paramsSerializer = function(params) {
